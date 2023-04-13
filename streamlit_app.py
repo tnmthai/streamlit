ca803a7a5238faf86b2a2232d16cd90e5f7af700
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-04-14 09:08:19 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ markdown = """
 
 st.markdown(markdown)
 
-m = leafmap.Map(minimap_control=True, center=(-43.525650, 172.639847), zoom=5.25)
+m = leafmap.Map(minimap_control=True, center=(-43.525650, 172.639847), zoom=6)
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
