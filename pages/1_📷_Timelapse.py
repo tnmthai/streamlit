df1b1494445fb4b7a9dbc62e061a742e88368bb9
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-04-14 10:06:51 +1200

    add shp

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -147,7 +147,7 @@ goes_rois = {
 # }
 
 import geopandas as gpd
-shapefile = gpd.read_file("nzshp/nzl.shp")
+shapefile = gpd.read_file("../data/nzshp/nzl.shp")
 
 features = []
 for i in range(shapefile.shape[0]):
