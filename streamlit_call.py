58ee225c45e2f2e07406ff34d9d42dab84ba4d4b
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2023-04-14 14:20:50 +1200

    Update streamlit_call.py

--- streamlit_call.py
+++ streamlit_call.py
@@ -2,7 +2,7 @@ from subprocess import Popen
 
 
 def load_jupyter_server_extension(nbapp):
-    """serve the streamlit app"""
+    """serve the streamlit Home"""
     Popen(
         [
             "streamlit",
